1ca388e3e41fb9570e0353f2a449266fd4d8f2fa
Your Name <you@example.com>
2022-02-28 17:02:09 +0330

    Adding sqlalchemy structure

--- authz/authz.py
+++ authz/authz.py
@@ -1,6 +1,12 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from authz.config import Config
+
+db = SQLAlchemy()
+mg = Migrate()
 
 apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
 apiv1 = Api(apiv1_bp) 
@@ -9,5 +15,7 @@ from authz import resource
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    db.init_app(app)
+    mg.init_app(app)
     app.register_blueprint(apiv1_bp)
     return app
--- authz/config/config.py
+++ authz/config/config.py
@@ -1,6 +1,9 @@
 from os import environ
 
 class Config:
+    
+    ################# Application Config #######################
+    
     ENV =  environ.get("TECHLAND_AUTHZ_ENV","production")
 
     DEBUG = bool(int(environ.get("TECHLAND_AUTHZ_DEBUG","0")))
@@ -8,4 +11,13 @@ class Config:
     TESTING = bool(int(environ.get("TECHLAND_AUTHZ_TESTING","0")))
 
     SECRET_KEY = environ.get("TECHLAND_AUTHZ_SECRET_KEY","HARD-HARD-HARD-fdgfthtr")
+
+    ################# Database Config #######################
+
+    SQLALCHEMY_DATABASE_URI = environ.get("TECHLAND_AUTHZ_DATABASE_URI",None)
+    SQLALCHEMY_ECHO = DEBUG
+    SQLALCHEMY_RECORD_QUERIES = DEBUG
+    SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
+
+
 
