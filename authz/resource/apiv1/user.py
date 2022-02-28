3d8882f2eaf645a86f7f663b7ab9dd7ae6fe6d3e
Your Name <you@example.com>
2022-02-28 13:03:39 +0330

    Adding user resource and user controller prototype

---
+++ authz/resource/apiv1/user.py
@@ -0,0 +1,34 @@
+from flask_restful import Resource
+
+from authz.controller.apiv1 import UserController
+class UserResource(Resource):
+    def get(self, user_id=None):
+        """
+        GET /users --> Get list of users.
+        GET /users/<user_id> --> Get user 
+        """
+        if user_id is None:
+            return UserController.get_users() # Get list of user
+        else: 
+            UserController.get_user(user_id)  # Get user
+
+    def post(self):
+        """
+        POST /users --> create new user.
+        POST /users/<user_id> --> Not Allowed
+        """
+        return UserController.create_user()   # Create user
+
+    def patch(self, user_id):
+        """
+        PATCH /users --> Not Allowed 
+        PATCH /users/<user_id> --> Update  user.
+        """
+        UserController.update_user(user_id)  # Update user
+
+    def delete(self, user_id):
+        """
+        DELETE /users --> Not Allowed
+        DELETE /users/<user_id> --> Delete user
+        """
+        UserController.delete_user(user_id)  # Delete user
