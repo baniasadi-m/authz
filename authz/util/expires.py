c217ea96913d1bd56dedbc75fc111182664195b4
Your Name <you@example.com>
2022-03-01 09:29:36 +0330

    Adding now and user_expires_at function to utils

---
+++ authz/util/expires.py
@@ -0,0 +1,7 @@
+from datetime import timedelta
+
+from authz.config import Config
+from authz.util import now
+
+def user_expires_at():
+    return now() + timedelta(days=Config.USER_DEFAULT_EXPIRY_TIME)
